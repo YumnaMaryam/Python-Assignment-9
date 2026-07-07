4d2cb203339cd0b903055b4341efee36837e9bd6
Yumna Maryam <maryamyumna89@gmail.com>
2026-07-07 14:43:00 +0500

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+print("Q1_Student Profile System:")
+Student = {
+     "Name" : "Yumna Maryam",
+     "Age" : 18,
+     "City" : "Karachi",
+     "Hobbies":["Reading","Cooking","Coding"],
+     "Skills":["Driving","Designing"]
+}
+print("Student Name:",Student["Name"])
+print("First Hobby:",Student["Hobbies"][0])
+print("Skills:",Student["Skills"])
+
+print("Q2_Students Marks System:")
+Student = {
+
+}
